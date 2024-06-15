81e142b8b2190955911031a631ad1bde9daf7068
Celeite <contactceleite@gmail.com>
2024-06-15 12:52:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# BlingBling Addons Routing
+*Created by [BlingBling](https://github.com/blingblingdeveloper) and [Celeite](https://github.com/CeleiteCode)*
+
+## Introduction:
+BlingBling Addons Routing is a routing utility [ChatTriggers](https://www.chattriggers.com/) module for Hypixel Skyblock.  
+Due to using creative-only features it's been denied for verification by the ChatTriggers team.  
+BlingBling Addons Routing **requires** [BlingBling Addons](https://github.com/blingblingdeveloper/BlingBlingAddons) to work.  
+
+## Features:
+- Etherwarp in singleplayer
+- Set all waypoint blocks quickly via `/swp [minecraft:blockid]`
+
+## Discord:
+Join the [Miner's Heaven](https://discord.gg/BBve6qaUqf) discord for more information.
---
+++ index.js
@@ -0,0 +1,48 @@
+import { drawBlock } from "../BlingBlingAddons/util/render";
+import Route from "../BlingBlingAddons/util/Route";
+
+// ===== etherwarp =====
+
+let castedBlock;
+
+register('playerInteract', () => {
+    if (Player.getHeldItem()?.getName()?.includes("Wooden Pickaxe") && Client.getMinecraft().field_71442_b.func_178889_l().func_77145_d()) { //2nd part detects if creative mode
+        if (!castedBlock) return;
+        if (castedBlock.valid) {
+            ChatLib.command(`gamerule sendCommandFeedback false`, false);
+            ChatLib.command(`tp ${castedBlock.pos.x} ${castedBlock.pos.y + 1} ${castedBlock.pos.z}`, false);
+            World.playSound("mob.enderdragon.hit", 1, 1);
+        }
+    }
+})
+
+register('renderWorld', () => {
+    if (Player.getHeldItem()?.getName()?.includes("Wooden Pickaxe") && Client.getMinecraft().field_71442_b.func_178889_l().func_77145_d()) {
+        castedBlock = rayTrace(57);
+        if (!castedBlock) return;
+        if (World.getBlockAt(castedBlock.pos.up(1)).type.getRegistryName() == "minecraft:air" && World.getBlockAt(castedBlock.pos.up(2)).type.getRegistryName() == "minecraft:air") {
+            castedBlock.valid = true;
+            drawBlock(castedBlock.pos, [156, 0, 0, 255], true);
+        }
+    } else {
+        castedBlock = null;
+    }
+})
+
+const rayTrace = (dist) => {
+    let rayBlockPos = Player.getPlayer().func_174822_a(dist, Tessellator.getPartialTicks())?.func_178782_a();
+    let blockAt = World.getBlockAt(rayBlockPos.func_177958_n(), rayBlockPos.func_177956_o(), rayBlockPos.func_177952_p());
+
+    return blockAt.type.getRegistryName() === "minecraft:air" ? null : { pos: blockAt.pos, valid: false };
+}
+
+// ===== setblock =====
+
+register('command', (message) => {
+    if (Client.getMinecraft().field_71442_b.func_178889_l().func_77145_d()) { // if creative mode
+        ChatLib.chat(`§d[BlingBling Addons] §fPlacing blocks!`);
+        for (let i = 0; i < Route.getRoute().length; i++) {
+            ChatLib.command(`setblock ${Route.getRoute()[i].x} ${Route.getRoute()[i].y} ${Route.getRoute()[i].z} ${message}`, false);
+        }
+    }
+}).setName('setblocks').setAliases(['sb']);
